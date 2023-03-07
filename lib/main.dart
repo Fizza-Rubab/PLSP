3f5b922ce1d4679280482166b55ffde27910b844
FizzaRubab <124028484+FizzaRubab@users.noreply.github.com>
2023-03-08 00:41:55 +0500

    initialize shared prefss, remove dead code

--- lib/main.dart
+++ lib/main.dart
@@ -15,65 +15,12 @@ import 'coming.dart';
 import 'arrived.dart';
 import 'homepage.dart';
 import 'display.dart';
+import 'shared.dart';
 
 
-// const Profile_Name = TextStyle(
-//   fontWeight: FontWeight.bold,
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 20,
-//   letterSpacing: 0,
-// );
-//
-// const role_style = TextStyle(
-//   fontStyle: FontStyle.italic,
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 15,
-//   letterSpacing: 0,
-// );
-//
-// const text_field = TextStyle(
-//   fontStyle: FontStyle.italic,
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 18,
-//   letterSpacing: 0,
-//   fontWeight: FontWeight.normal,
-// );
-//
-// const not_pressed = TextStyle(
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 18,
-//   letterSpacing: 0,
-//   fontWeight: FontWeight.normal,
-// );
-// const radio_buttons = TextStyle(
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 15,
-//   letterSpacing: 0,
-//   fontWeight: FontWeight.normal,
-// );
-// var linkText = TextStyle(
-//   color: Color.fromRGBO(255, 0, 95, 1),
-//   fontFamily: 'Poppins',
-//   fontSize: 10,
-//   letterSpacing: 0,
-//   fontWeight: FontWeight.normal,
-//   height: 0,
-// );
-// var disclaimerText = TextStyle(
-//   color: Colors.black45,
-//   fontFamily: 'Poppins',
-//   fontSize: 10,
-//   letterSpacing: 0,
-//   fontWeight: FontWeight.normal,
-//   height: 0,
-// );
 
 void main() {
+  sharedPrefInit();
   runApp(
     MaterialApp(
       home:
@@ -82,48 +29,3 @@ void main() {
     ),
   );
 }
-
-//-----------------------------------------------
-//-----------------------------------------------
-// class MapSample extends StatefulWidget {
-//   @override
-//   State<MapSample> createState() => MapSampleState();
-// }
-//
-// class MapSampleState extends State<MapSample> {
-//   Completer<GoogleMapController> _controller = Completer();
-//
-//   static final CameraPosition _kGooglePlex = CameraPosition(
-//     target: LatLng(37.42796133580664, -122.085749655962),
-//     zoom: 14.4746,
-//   );
-//
-//   static final CameraPosition _kLake = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(37.43296265331129, -122.08832357078792),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       body: GoogleMap(
-//         mapType: MapType.hybrid,
-//         initialCameraPosition: _kGooglePlex,
-//         onMapCreated: (GoogleMapController controller) {
-//           _controller.complete(controller);
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton.extended(
-//         onPressed: _goToTheLake,
-//         label: Text('To the lake!'),
-//         icon: Icon(Icons.directions_boat),
-//       ),
-//     );
-//   }
-//
-//   Future<void> _goToTheLake() async {
-//     final GoogleMapController controller = await _controller.future;
-//     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-//   }
-// }
