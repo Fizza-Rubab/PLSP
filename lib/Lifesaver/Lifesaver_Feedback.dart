093989ec98995c3c94ad928ad46fbcb3edb61d7c
Shamsa-Hafeez <60125155+Shamsa-Hafeez@users.noreply.github.com>
2023-04-12 16:02:21 +0500

    Very minor changes in UI

--- lib/Lifesaver/Lifesaver_Feedback.dart
+++ lib/Lifesaver/Lifesaver_Feedback.dart
@@ -5,7 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps/Alert/Thankyou.dart';
 import '../Home/Citizen.dart';
 import '../input_design.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'appbar.dart';
+// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
 
 class Lifesaver_Feedback extends StatefulWidget {
@@ -23,57 +24,37 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
   bool _didPerformLifeSavingIntervention = false;
   bool _didMedicalHelpArrive = false;
 
-  final TextEditingController _feedbackController = TextEditingController();
+  TextStyle question_style = GoogleFonts.poppins(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 0,
+                color: Colors.grey.shade800,
+              ); 
 
-  void _addEntry() {
-    setState(() {
-      _entries.add(_textController.text);
-      _textController.clear();
-    });
-  }
+  TextStyle option_style = GoogleFonts.poppins(
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0,
+                    color: Colors.grey.shade800,
+                  ); 
 
-  void _deleteEntry(int index) {
-    setState(() {
-      _entries.removeAt(index);
-    });
-  }
-
-  double _rating = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        // iconTheme: IconThemeData(color: appbar_icon_color),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Text(
-          "Post Emergency Form",
-          style: GoogleFonts.poppins(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-            letterSpacing: 0,
-            color: Colors.redAccent,
-          ),
-        ),
-        centerTitle: true,
-      ),
+      appBar: SimpleAppBar("Post Emergency form"),
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 20.0),
             Text(
               'Did you perform any life-saving intervention?',
-              style: GoogleFonts.lato(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0,
-                color: Colors.grey.shade800,
-              ),
+              style: question_style
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Radio(
                   value: true,
@@ -87,11 +68,7 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                 ),
                 Text(
                   'Yes',
-                  style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),
+                  style: option_style
                 ),
                 Radio(
                   value: false,
@@ -103,23 +80,17 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                   },
                   activeColor: Colors.redAccent,
                 ),
-                Text('No',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'No',
+                  style:option_style
+                ),
               ],
             ),
+
             SizedBox(height: 20.0),
             Text(
               'What was the life-saving intervention you performed?',
-              style: GoogleFonts.lato(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0,
-                color: Colors.grey.shade800,
-              ),
+              style: question_style
             ),
             Row(
               children: <Widget>[
@@ -133,12 +104,10 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                   },
                   activeColor: Colors.redAccent,
                 ),
-                Text('CPR',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'CPR',
+                  style: option_style
+                ),
                 Radio(
                   value: 'Bleeding Control',
                   groupValue: _lifeSavingIntervention,
@@ -149,12 +118,10 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                   },
                   activeColor: Colors.redAccent,
                 ),
-                Text('Bleeding Control',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'Bleeding Control',
+                  style: option_style
+                ),
                 Radio(
                   value: 'Both',
                   groupValue: _lifeSavingIntervention,
@@ -165,23 +132,16 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                   },
                   activeColor: Colors.redAccent,
                 ),
-                Text('Both',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'Both',
+                  style: option_style
+                ),
               ],
             ),
             SizedBox(height: 20.0),
             Text(
               'Did the medical help arrive at the location or was the patient taken to the hospital?',
-              style: GoogleFonts.lato(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0,
-                color: Colors.grey.shade800,
-              ),
+              style: question_style
             ),
             Row(
               children: <Widget>[
@@ -194,14 +154,11 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                     });
                   },
                   activeColor: Colors.redAccent,
-
                 ),
-                Text('Yes',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'Yes',
+                  style: option_style
+                ),
                 Radio(
                   value: false,
                   groupValue: _didMedicalHelpArrive,
@@ -211,56 +168,59 @@ class _Lifesaver_FeedbackState extends State<Lifesaver_Feedback> {
                     });
                   },
                   activeColor: Colors.redAccent,
-
                 ),
-                Text('No',
-                style: GoogleFonts.poppins(
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0,
-                    color: Colors.grey.shade800,
-                  ),),
+                Text(
+                  'No',
+                  style: option_style
+                ),
               ],
             ),
             SizedBox(height: 20.0),
             Text(
               'Any other feedback?',
-              style: GoogleFonts.lato(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                letterSpacing: 0,
-                color: Colors.grey.shade800,
-              ),
+              style: question_style
             ),
             TextField(
-              maxLines: 3,
-              decoration: buildInputDecoration(Icons.edit, "", border: BorderRadius.all(Radius.circular(20))),
+              maxLines: 2,
+              decoration: buildInputDecoration(
+                Icons.edit,
+                "",
+                border: BorderRadius.all(Radius.circular(20)),
+                
+              ),
             ),
             // TextField(controller: _feedbackController, maxLines: 2),
             SizedBox(
               height: 20,
             ),
             Center(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    backgroundColor: Colors.redAccent,
+                    fixedSize:
+                        Size(MediaQuery.of(context).size.width / 2.4, 30),
+                    textStyle: const TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        color: Colors.white),
                   ),
-                  backgroundColor: Colors.redAccent,
-                  fixedSize: Size(MediaQuery.of(context).size.width / 2.4, 30),
-                  textStyle: const TextStyle(
-                      fontSize: 18, fontFamily: 'Poppins', color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => ThankYouScreen()));
-                },
-                child: Text(
-                  'Submit',
-                  style: GoogleFonts.poppins(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 1.0,
-                      color: Colors.grey.shade100),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ThankYouScreen()));
+                  },
+                  child: Text(
+                    'Submit',
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.0,
+                        color: Colors.grey.shade100),
+                  ),
                 ),
               ),
             ),
