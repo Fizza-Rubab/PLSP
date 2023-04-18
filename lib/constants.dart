34d64a35e639f7f44a06a19ff5940d5b202ece43
Shamsa-Hafeez <60125155+Shamsa-Hafeez@users.noreply.github.com>
2023-04-18 16:24:27 +0500

    To Do: Shift all UI vars in sep file

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 const PrimaryColor = Colors.redAccent;
 
+var titleFontStyle = GoogleFonts.poppins(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    letterSpacing: 0,
+    color: Colors.black);
 
+var generalfontStyle = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    color: Colors.black38);
 
-var titleFontStyle = GoogleFonts.poppins(fontSize: 20, 
-                fontWeight: FontWeight.bold, 
-                letterSpacing: 0, 
-                color: Colors.black); 
-
-var generalfontStyle = GoogleFonts.poppins(fontSize: 14, 
-                fontWeight: FontWeight.w500, 
-                letterSpacing: 0, 
-                color: Colors.black38); 
+var urlfontStyle = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+    color: Colors.redAccent,
+    decoration: TextDecoration.underline);
 
 const double defaultPadding = 16.0;
-
 
 class ApiConstants {
   // static String baseUrl = 'http://kaavish2023.pythonanywhere.com';
@@ -29,7 +34,4 @@ class ApiConstants {
   static String loginEndpoint = '/login/';
   static String signupEndpoint = '/register';
   static String incidentEndpoint = '/incident';
-
-
-
 }
