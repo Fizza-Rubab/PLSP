a0a32c898bd8727528051d4d2e95186eb424227e
Fizza-Rubab <fr06161@st.habib.edu.pk>
2023-09-23 22:01:06 +0500

    updated url endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-
 from rest_framework_simplejwt import views as jwt_views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='PLSP Backend API')
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('', include('api.urls')),
-    path('dashboard/', include('dashboard.urls'))
-]
+    path('', include('dashboard.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
